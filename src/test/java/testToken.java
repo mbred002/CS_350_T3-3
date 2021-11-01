e689390d39bcc379e58f8584e66dda745e85eadc
Matthew <mbred002@odu.edu>
2021-10-31 23:49:16 -0400

    Integrated project with gradle and created first test

---
+++ src/test/java/testToken.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat; 
+import static org.hamcrest.Matchers.*;
+
+public class testToken
+{
+	@Test
+	public void testDetermineToken
+	{
+		
+	}
+	
+	}
