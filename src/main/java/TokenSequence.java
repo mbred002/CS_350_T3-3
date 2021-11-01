a4a87085c998936d3cb1bd531e5ded0e8cc8f060
Matthew <mbred002@odu.edu>
2021-10-31 23:18:52 -0400

    Publishing TokenSequence to current file structure

--- src/main/java/TokenSequence.java
+++ src/main/java/TokenSequence.java
@@ -13,7 +13,26 @@ public class TokenSequence {
 	
 	Token returnToken(int tokenIndex) {
 		return sequence.elementAt(tokenIndex);
+		
 	}
+	
+	//////// Count each token type /////////
+	
+	int CountTokenType(String TypeX){
+		
+		int i = 0, num = sequence.size();
+		int count =0;
+		for (i =0; i < num; i++ );
+		{
+			if (sequence.get(i).getTokenType().equals(TypeX))
+			{
+				this.numTokens++;
+				count++;
+			}
+		}
+		return count;
+	};
+	
 	
 	
 	
