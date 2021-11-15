d190950b1ecb780c81be2456edd62d8976dfd44c
thast002 <thast002@odu.edu>
2021-11-15 16:18:11 -0500

    testCPPSourceFile constructor unit test

--- src/test/java/testCPPSourceFile.java
+++ src/test/java/testCPPSourceFile.java
@@ -8,12 +8,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class testCPPSourceFile {
-//Ill take care of these three -shayla 
 	@Test 
 	public void testCPPSourceFile()
-	{
-	//constructor 	
-		
+	{	
+		CPPSourceFile testFilFe = new CPPSourceFile("src/main/resources/helloworld.cpp");
+		assertTrue(testFilFe.getLineNum()==0);
+		assertTrue(testFilFe.getColumnNum()==0);
 	}
 	@Test
 	public void testTokenize()
@@ -36,12 +36,12 @@ public class testCPPSourceFile {
 	@Test
 	public void testGetNextToken()
 	{
-		CPPSourceFile testFile = new CPPSourceFile("src/main/resources/helloworld.cpp");
+		CPPSourceFile testFilFe = new CPPSourceFile("src/main/resources/helloworld.cpp");
 		try {
-			Scanner scan = new Scanner(testFile.getFile());
+			Scanner scan = new Scanner(testFilFe.getFile());
 			String testStr = scan.nextLine();
 
-			Token nextToken = testFile.getNextToken(testStr, 1, 0);
+			Token nextToken = testFilFe.getNextToken(testStr, 1, 0);
 			
 			assertTrue(nextToken.getLexeme().equals("#include<iostream>"));
 			assertTrue(nextToken.getTokenType().equals("preprocessor-op"));
